8452e44dc89e848b69e9c53e4be58d3bddbeb425
Kevin <k.mary@live.be>
2020-11-12 14:11:53 +0100

    search jobs with livewire and alpinejs

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,17 +1,24 @@
 <header class="flex justify-between items-center py-5">
     <div>
-        LOGO
+        <a href="{{ route('jobs.index') }}">
+            <img src="{{ asset('images/logo.png') }}" alt="logo" class="w-48">
+        </a>        
     </div>
 
     <nav>
-        <a href="{{ route('jobs.index') }}" class="mr-5 hover:text-green-500">Nos missions</a>
+        <livewire:search />
+        <a href="{{ route('jobs.index') }}" class="mr-5 {{ request()->routeIs('jobs.*') ? 'text-green-500 font-semibold' : '' }} hover:text-green-500">
+            Nos missions
+        </a>
         @guest
-            <a href="{{ route('login') }}" class="mr-5 hover:text-green-500">Se connecter</a>
-            <a href="{{ route('register') }}" class="mr-5 hover:text-green-500">S'enregistrer</a>
+            <a href="{{ route('login') }}" class="mr-5 {{ request()->routeIs('login') ? 'text-green-500 font-semibold' : '' }} hover:text-green-500">Se connecter</a>
+            <a href="{{ route('register') }}" class="mr-5 {{ request()->routeIs('register') ? 'text-green-500 font-semibold' : '' }} hover:text-green-500">S'enregistrer</a>
         @else
-            <a href="{{ route('home') }}" class="mr-5 hover:text-green-500">Tableau de bord</a>
+            <a href="{{ route('home') }}" class="mr-5 {{ request()->routeIs('home') ? 'text-green-500 font-semibold' : '' }} hover:text-green-500">Tableau de bord</a>
             <a href="{{ route('logout') }}" class="hover:text-green-500" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Se déconnecter</a>
-            <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display:none;">@csrf</form>
+            <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display:none;">
+                @csrf
+            </form>
         @endguest
     </nav>
 </header>
